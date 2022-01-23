5bd095126d6d517164a3cac755fb28af0bad0d86
TurnerRussell50 <54976031+TurnerRussell50@users.noreply.github.com>
2022-01-23 13:25:51 -0700

    Update displayMousePosition.py

--- displayMousePosition.py
+++ displayMousePosition.py
@@ -1,4 +1,4 @@
-#Run this to see WHERE, in XY cpoordinates, the mouse click event will occur
+#Run this to see WHERE, in XY cpoordinates, the mouse click event will occur, then input these values into Bot.py
 
 import pyautogui
 pyautogui.displayMousePosition()
